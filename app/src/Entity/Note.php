b58816030d1ed05351eb7c8992a6bc0cbcad788e
Michał Dudek <michalpaweldudek@gmail.com>
2024-05-22 17:50:37 +0200

    note.php corrections

--- app/src/Entity/Note.php
+++ app/src/Entity/Note.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\NoteRepository;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -10,73 +11,83 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name: 'notes')]
 class Note
 {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $createdAt = null;
+    #[ORM\Column(type: 'datetime_immutable')]
+    private ?DateTimeImmutable $createdAt;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $updatedAt = null;
+    #[ORM\Column(type: 'datetime_immutable')]
+    private ?DateTimeImmutable $updatedAt;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
     private ?string $title = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(type: 'text')]
     private ?string $content = null;
+
 
     public function getId(): ?int
     {
         return $this->id;
-    }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    }//end getId()
+
+
+    public function getCreatedAt(): ?DateTimeImmutable
     {
         return $this->createdAt;
-    }
+    }//end getCreatedAt()
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): static
+
+    public function setCreatedAt(?DateTimeImmutable $createdAt): void
     {
         $this->createdAt = $createdAt;
+    }//end setCreatedAt()
 
-        return $this;
-    }
 
-    public function getUpdatedAt(): ?\DateTimeInterface
+    public function getUpdatedAt(): ?DateTimeImmutable
     {
         return $this->updatedAt;
-    }
 
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    }//end getUpdatedAt()
+
+
+    public function setUpdatedAt(?DateTimeImmutable $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
 
-        return $this;
-    }
+    }//end setUpdatedAt()
+
 
     public function getTitle(): ?string
     {
         return $this->title;
-    }
+    }//end getTitle()
 
-    public function setTitle(string $title): static
+
+    public function setTitle(string $title): void
     {
         $this->title = $title;
 
-        return $this;
-    }
+    }//end setTitle()
+
 
     public function getContent(): ?string
     {
         return $this->content;
-    }
 
-    public function setContent(string $content): static
+    }//end getContent()
+
+
+    public function setContent(string $content): void
     {
         $this->content = $content;
 
-        return $this;
-    }
-}
+    }//end setContent()
+
+
+}//end class
