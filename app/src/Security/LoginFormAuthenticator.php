d3408adbed0996974f76427022c6b402f290d3f4
Michał Dudek <michalpaweldudek@gmail.com>
2024-05-27 17:50:33 +0200

    added users and made /hello public

--- app/src/Security/LoginFormAuthenticator.php
+++ app/src/Security/LoginFormAuthenticator.php
@@ -37,7 +37,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
      *
      * @const string
      */
-    private const DEFAULT_ROUTE = 'task_index';
+    private const DEFAULT_ROUTE = 'note_index';
 
 
     /**
