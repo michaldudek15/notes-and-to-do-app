8499db2d41ee01d0ed0fade4e83810f043ad1da9
Michał Dudek <michalpaweldudek@gmail.com>
2026-05-23 11:34:28 +0200

    add tests for index guest and user

--- app/tests/Controller/CategoryControllerTest.php
+++ app/tests/Controller/CategoryControllerTest.php
@@ -7,6 +7,8 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Category;
+use App\Entity\Enum\UserRole;
+use App\Entity\User;
 use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,18 +17,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CategoryControllerTest extends WebTestCase
 {
-    public const TEST_ROUTE = '/category';
+    public const string TEST_ROUTE = '/category';
 
     private KernelBrowser $httpClient;
-
-    private ?EntityManagerInterface $entityManager;
+    private EntityManagerInterface $entityManager;
+    private UserRepository $userRepository;
+    private CategoryRepository $categoryRepository;
 
     /**
      * Set up test environment before each test.
      */
     protected function setUp(): void
     {
-        $this->client = static::createClient();
+        $this->httpClient = static::createClient();
+        $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
         $this->userRepository = static::getContainer()->get(UserRepository::class);
         $this->categoryRepository = static::getContainer()->get(CategoryRepository::class);
     }
@@ -37,10 +41,48 @@ class CategoryControllerTest extends WebTestCase
      */
     public function testIndexGuest(): void
     {
-        $this->client->request('GET', self::TEST_ROUTE);
+        $this->httpClient->request('GET', self::TEST_ROUTE);
 
-        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(302, $this->httpClient->getResponse()->getStatusCode());
     }
 
+
+    public function testIndexUser(): void
+    {
+        $user = $this->createUser([UserRole::ROLE_USER->value]);
+
+        $this->httpClient->loginUser($user);
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+
+        $this->assertEquals(200, $this->httpClient->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Create user helper.
+     */
+    private function createUser(array $roles): User
+    {
+        $passwordHasher = static::getContainer()
+            ->get('security.password_hasher');
+
+        $user = new User();
+        $user->setEmail('user'.uniqid().'@example.com');
+        $user->setRoles($roles);
+        //$user->setBlocked(false);
+
+        $user->setPassword(
+            $passwordHasher->hashPassword(
+                $user,
+                'password'
+            )
+        );
+
+        $userRepository = static::getContainer()
+            ->get(UserRepository::class);
+
+        $userRepository->save($user);
+
+        return $user;
+    }
 
 }
