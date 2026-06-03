941651c6db8551d5d1c12fd0246e3cd4c455f3f2
Michał Dudek <michalpaweldudek@gmail.com>
2026-06-03 09:05:51 +0200

    Expand **UserServiceTest** with test cases for delete, pagination, and repository delegation.

--- app/tests/Service/UserServiceTest.php
+++ app/tests/Service/UserServiceTest.php
@@ -5,13 +5,16 @@
  */
 
 namespace App\Tests\Service;
+
 use App\Entity\Enum\UserRole;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Service\UserService;
 use App\Service\UserServiceInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\EntityManagerInterface;
-use PHPUnit\Framework\TestCase;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserServiceTest extends KernelTestCase
@@ -25,7 +28,7 @@ class UserServiceTest extends KernelTestCase
         $this->entityManager = $container->get('doctrine.orm.entity_manager');
         $this->userService = $container->get(UserService::class);
     }
-    public function testSave(): void
+    public function testSaveUserToDatabase(): void
     {
         // given
         $expectedUser = new User();
@@ -48,5 +51,74 @@ class UserServiceTest extends KernelTestCase
             ->getSingleResult();
 
         $this->assertEquals($expectedUser, $resultUser);
+    }
+
+    public function testDeleteRemovesUserFromDatabase(): void
+    {
+        // given
+        $user = new User();
+        $user->setEmail('delete'.uniqid().'@example.com');
+        $user->setPassword('password');
+        $user->setRoles([UserRole::ROLE_USER->value]);
+
+        $this->userService->save($user);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $id = $user->getId();
+
+        // when
+        $this->userService->delete($user);
+
+        // then
+        self::assertNull($this->entityManager->find(User::class, $id));
+
+    }
+
+    public function testGetPaginatedListReturnsPagination(): void
+    {
+        // given
+        $user = new User();
+        $user->setEmail('paginate'.uniqid().'@example.com');
+        $user->setPassword('password');
+        $user->setRoles([UserRole::ROLE_USER->value]);
+
+        $this->userService->save($user);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        // when
+        $pagination = $this->userService->getPaginatedList(1);
+
+        // then
+        self::assertInstanceOf(PaginationInterface::class, $pagination);
+        self::assertSame(1, $pagination->getCurrentPageNumber());
+        self::assertSame(10, $pagination->getItemNumberPerPage());
+        self::assertGreaterThanOrEqual(1, $pagination->getTotalItemCount());
+
+    }
+
+    public function testSaveDelegatesToRepository(): void
+    {
+        // given
+        $user = new User();
+        $user->setEmail('delegate@example.com');
+        $user->setPassword('password');
+        $user->setRoles([UserRole::ROLE_USER->value]);
+
+        // when
+        $userRepository = $this->createMock(UserRepository::class);
+        $userRepository->expects(self::once())
+            ->method('save')
+            ->with(self::identicalTo($user));
+
+        $service = new UserService(
+            $userRepository,
+            $this->createMock(PaginatorInterface::class),
+        );
+
+        $service->save($user);
     }
 }
