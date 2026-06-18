6b792d0c2b45034c2a822614c5e9619a965206a6
Michał Dudek <michalpaweldudek@gmail.com>
2026-06-18 19:19:55 +0200

    check_code result

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -126,7 +126,8 @@ class SecurityController extends AbstractController
     /**
      * Change email.
      *
-     * @param Request $request Request
+     * @param Request        $request Request
+     * @param UserRepository $userRepository User repository
      *
      * @return Response Response
      */
