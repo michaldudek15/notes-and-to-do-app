c29b6a033efce7c01534ab56b1fce314ad5205b3
Michał Dudek <michalpaweldudek@gmail.com>
2026-06-18 19:27:31 +0200

    check_code result

--- app/tests/Repository/UserRepositoryTest.php
+++ app/tests/Repository/UserRepositoryTest.php
@@ -36,7 +36,7 @@ class UserRepositoryTest extends AbstractWebTestCase
      */
     public function testUpgradePasswordThrowsForUnsupportedUser(): void
     {
-        $unsupportedUser = new class() implements PasswordAuthenticatedUserInterface {
+        $unsupportedUser = new class () implements PasswordAuthenticatedUserInterface {
             /**
              * Returns a password required by the interface.
              */
